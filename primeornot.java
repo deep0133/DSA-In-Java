4dede55c5e1dc22a38b13d579b8e71a3c51e7fb0
deep0133 <79748580+deep0133@users.noreply.github.com>
2021-06-07 10:13:05 +0530

    Optimise

--- primeornot.java
+++ primeornot.java
@@ -7,14 +7,14 @@ public class primeornot {
 
   public static void main(String[] args) {
    Scanner sc= new Scanner(System.in);
-   int a = sc.nextInt();
+   int n = sc.nextInt();
   boolean isprime = true;
-   for( int i=2; i<a; i++){
-     if( a % i ==0){
+   for( int i=2; i*i<=n; i++){
+     if( n % i ==0){
        isprime = false;
        break;
    }
-         }if( a<2) isprime=false;
+         }if( n<2) isprime=false;
      System.out.println("is prime ="+
      isprime);
 
